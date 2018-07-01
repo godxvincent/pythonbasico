3058658a6a29654bf620741dcbd85e0ccb6a624c
godxvincent <godxvincent@gmail.com>
2018-07-01 18:47:18 -0500

    Se adicionan otros tipos de popups al archivo popups.py

--- popups.py
+++ popups.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox as MessageBox
+from tkinter import colorchooser as ColorChooser
+from tkinter import filedialog as FileDialog
 
 
 def test():
@@ -15,9 +17,23 @@ def test():
     # resultado = MessageBox.askyesnocancel("Salir", "Esta seguro que desea salir sin guardar?")
     # if resultado:  # true o false
     #     root.destroy()
-    resultado = MessageBox.askretrycancel("Reintentar", "No hubo conexión")
-    if resultado:  # true o false
-        root.destroy()
+    # resultado = MessageBox.askretrycancel("Reintentar", "No hubo conexión")
+    # if resultado:  # true o false
+    #     root.destroy()
+    # color = ColorChooser.askcolor(title="Elige un color!")
+    # print(color)
+    # askopenfilename Devuele una ruta
+    # ruta = FileDialog.askopenfilename(title="Abrir un fichero", initialdir="C:",
+    #                                   filetypes=(("Archivos de texto", "*.txt"),
+    #                                              ("Archivos PDF", "*.pdf"),
+    #                                              ("Todos los archivos", "*.*")))
+    # print(ruta)
+    # asksaveasfile Devuele un fichero
+    fichero = FileDialog.asksaveasfile(
+        title="Guardar un fichero", defaultextension=".txt", mode="w")
+    if fichero is not None:
+        fichero.write("Esto es un texto de prueba")
+        fichero.close()
 
 
 # Configurando la raiz de una aplicación de escritorio
